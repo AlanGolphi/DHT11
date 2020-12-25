7b4260ea424eb8313bc4ec33f3c98e70d7f84419
AlanGolphi <alangolphi@163.com>
2020-12-25 23:20:44 +0800

    delete all init files

--- hardware/EXTI/exti.h
+++
@@ -1,9 +0,0 @@
-#ifndef  _EXTI_H
-#define  _EXTI_H
-
-#include "stm32f10x.h"
-
-void EXTIX_Init(void);
-
-#endif
-
--- project/common.d
+++
@@ -1,8 +0,0 @@
-.\common.o: ..\Utils\common.c
-.\common.o: ..\Utils\common.h
-.\common.o: D:\KeilForArm\ARM\ARMCC\Bin\..\include\stdint.h
-.\common.o: D:\KeilForArm\ARM\ARMCC\Bin\..\include\stdbool.h
-.\common.o: D:\KeilForArm\ARM\ARMCC\Bin\..\include\stdio.h
-.\common.o: D:\KeilForArm\ARM\ARMCC\Bin\..\include\string.h
-.\common.o: D:\KeilForArm\ARM\ARMCC\Bin\..\include\ctype.h
-.\common.o: D:\KeilForArm\ARM\ARMCC\Bin\..\include\stdlib.h
